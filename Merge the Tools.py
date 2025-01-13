bc8051f1264cc725ae74b964fc188a70dbbb6c04
G1Codes <jeevan983+github@gmail.com>
2025-01-14 01:17:43 +0530

    Add files via upload

---
+++ Merge the Tools.py
@@ -0,0 +1,46 @@
+'''
+String- *Merge the Tools!*
+
+Consider the following:
+A string, s, of length n.
+An integer, k, where k is a factor of n.
+We can split s into n/k subsegments where each subsegment, t(i), consists of a contiguous block of k characters in s.
+Then, use each t(i) to create string u(i) such that:
+The characters in u(i) are a subsequence of the characters in t(i).
+Any repeat occurrence of a character is removed from the string such that each character in u(i) occurs exactly once. 
+In other words, if the character at some index j in t(i) occurs at a previous index < j in t(i), then do not include the 
+character in string u(i).
+Given s and k, print n/k lines where each line i denotes string u(i).
+Input Format
+The first line contains a single string denoting s. 
+The second line contains an integer, k, denoting the length of each subsegment.
+Output Format
+Print n/k lines where each line i contains string u(i).
+Sample Input
+AABCAAADA
+3   
+Sample Output
+AB
+CA
+AD
+
+'''
+
+def merge_the_tools(string, k):
+    k=3
+    l=len(string)
+    
+    for i in range (0,l,k):
+        a= (string[i:i+k])
+        
+        str= ''
+        for char in a:
+            if char not in str:
+                str+=char
+        print(str)
+            
+    
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
